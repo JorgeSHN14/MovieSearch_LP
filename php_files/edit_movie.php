d631ca25735955a97bebbf18f8c561a4c341a105
JorgeSHN14 <joheniet@espol.edu.ec>
2024-01-14 22:51:20 -0500

    cambios de testing

--- php_files/edit_movie.php
+++ php_files/edit_movie.php
@@ -1,13 +1,15 @@
 <?php
 include 'send_request.php';
 include 'cont_op_p.php';
-$idMovieToDelete = 5;
+$idMovieToDelete = 1;
 $endpoint = 'http://127.0.0.1:8000/api/peliculas/' . $idMovieToDelete . '/';
 $dataToEdit = array(
-    'nombre' => 'Pasajeros',
-    'genero' => 'Ciencia Ficcion, Aventura',
+    'nombre' => 'Como si fuera la primera vez(50 first dates)',
+    'genero' => 'Comedia, Romance',
+    'elenco_principal' => 'Adam Samdler, Drew Barrymore, Allen Covert',
 );
 
 $context_options = contextOptionsP($dataToEdit,'PATCH');
 $response = sendRequest($endpoint,$context_options);
 echo $response;
+
--- php_files/remove_movie.php
+++ php_files/remove_movie.php
@@ -1,7 +1,7 @@
 <?php
 include 'send_request.php';
 include 'cont_op_delete.php';
-$idMovieToDelete = 5;
+$idMovieToDelete = 1;
 $endpoint = 'http://127.0.0.1:8000/api/peliculas/' . $idMovieToDelete . '/';
 $context_options = contextOptionsDelete();
 $response = sendRequest($endpoint, $context_options);
